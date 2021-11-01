4a54cf56b460324905c51d5c970d3892322ef60f
Ethan Stevenson <Mr.NavaStar@gmail.com>
2021-10-31 20:40:34 -1000

    Remove some unused code

--- src/main/java/mrnavastar/nocoin/util/BetterPlayerManager.java
+++ src/main/java/mrnavastar/nocoin/util/BetterPlayerManager.java
@@ -1,12 +1,9 @@
 package mrnavastar.nocoin.util;
 
 import com.mojang.authlib.GameProfile;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.util.UserCache;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -14,14 +11,6 @@ import static mrnavastar.nocoin.Nocoin.playerManager;
 import static mrnavastar.nocoin.Nocoin.userCache;
 
 public class BetterPlayerManager {
-
-    public static PlayerEntity getPlayer(UUID uuid) {
-        return playerManager.getPlayer(uuid);
-    }
-
-    public static PlayerEntity getPlayer(String name) {
-        return playerManager.getPlayer(name);
-    }
 
     public static String getName(UUID uuid) {
         Optional<GameProfile> gameProfile = userCache.getByUuid(uuid);
